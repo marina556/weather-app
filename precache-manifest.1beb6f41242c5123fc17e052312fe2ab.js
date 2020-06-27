9848e492053e66ceaad3a00701b62e1ded9cb28e
marina556 <marinamakram556@gmail.com>
2020-06-27 15:08:08 +0200

    Updates

---
+++ precache-manifest.1beb6f41242c5123fc17e052312fe2ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c729bf1f325675ff5d6f511d71412eb3",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ea7ba1a533f745227f3e",
+    "url": "/weather-app/static/css/main.d5aef841.chunk.css"
+  },
+  {
+    "revision": "8387b3da2f665b2c1bfa",
+    "url": "/weather-app/static/js/2.76aff883.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.76aff883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea7ba1a533f745227f3e",
+    "url": "/weather-app/static/js/main.26070b5b.chunk.js"
+  },
+  {
+    "revision": "6217e7c726a47d063da4",
+    "url": "/weather-app/static/js/runtime-main.8ce43dfd.js"
+  }
+]);
